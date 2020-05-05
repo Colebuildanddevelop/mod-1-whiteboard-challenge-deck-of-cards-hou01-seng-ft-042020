a37ae0d97e9f38af0e8e7fcd5fd05aa9401be994
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-05-05 13:23:04 -0500

    smells like spaghetti

--- solution.rb
+++ solution.rb
@@ -1,7 +1,47 @@
-class Deck
+require 'pry'
 
+class Deck
+  attr_accessor :cards
+  @@all = [] 
+  RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  SUITS = ["Hearts", "Clubs", "Diamonds", "Spades"]
+  def initialize 
+    @cards = []
+    r_count = 0 
+    s_count = 0
+    RANKS.length.times do 
+      SUITS.length.times do 
+        @cards << Card.new(RANKS[r_count], SUITS[s_count])
+        s_count > 2 ? s_count = 0 : s_count += 1
+      end 
+      r_count += 1 
+    end 
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def chose_card 
+    @cards.sample
+  end
 end
 
 class Card
+  attr_accessor :rank, :suit
+  @@all = []
+  def initialize(rank, suit) 
+    @rank = rank 
+    @suit = suit
+    @@all << self
+  end 
+
+  def self.all 
+    @@all
+  end 
 
 end
+
+binding.pry 
+0
